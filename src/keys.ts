d4c49309d896282b9396b9dfcade5d5d9e2ee486
EduardoBan <banueloscarrilloe@gmail.com>
2024-02-24 10:23:08 -0600

    first commit

---
+++ src/keys.ts
@@ -0,0 +1,11 @@
+
+export default {
+    database: {
+        host: 'monorail.proxy.rlwy.net',
+        user: 'root',
+        password: 'f1dHHfHdehb5FFD12dbcbhFf2ChG-F5E',
+        database: 'crepas_db',
+        timezone: '+00:00',
+        port: 21768   
+    }
+}
